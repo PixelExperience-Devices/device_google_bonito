1df1596d0be730ea215765852b0cb03c9f3e4f35
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-14 00:36:02 -0700

    b4s4: Set up proprietary files extraction & build vendor image
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Tue May 14 00:36:02 2019 -070
        b4s4: Import and tailor {extract-files,setup-makefiles}.sh
    
        This is a squash of the following:
    
        Author: Michael Bestas <mkbestas@lineageos.org>
        Date:   Sat Dec 19 02:53:35 2020 +0200
    
            b4s4: Switch to standalone extract utils
    
            Align with templates while we are at it
    
            Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4
    
        Author: Rashed Abdel-Tawab <rashed@linux.com>
        Date:   Tue May 14 00:36:02 2019 -0700
    
            b4s4: Import and tailor {extract-files,setup-makefiles}.sh
    
            * Partial pick of Id399faa404207ce018f0f0187c9576b61f5d4dc9.
    
            Change-Id: I866e015e3ba717ae7fc585dc56f118f3e251154c
    
    Change-Id: I231d822f364f57314009be4d08af993b97bf5705
    
    Author: Eamon Powell <eamonpowell@outlook.com>
    Date:   Sun Nov 1 20:00:06 2020 +1100
    
        b4s4: Initial Android 12 proprietary files
    
        Co-authored-by: Josh Chasky <jchasky@gmail.com>
        Co-authored-by: razorloves <razorloves@gmail.com>
        Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
        Co-authored-by: "Josh Fox (XlxFoXxlX)" <joshfox87@gmail.com>
        Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
        Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
        Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
        Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
        Change-Id: I7cf6964340a1e91377d951c5baf4ef81e9c91ce9
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sat May 25 23:41:28 2019 -0500
    
        b4s4: Build the vendor image
    
        Change-Id: I6e77335985526162433b1b879775a6fd89163c5e
    
    Author: Eamon Powell <eamonpowell@outlook.com>
    Date:   Tue Dec 29 10:16:40 2020 +1100
    
        b4s4: Add missing properties and packages
    
        Now that we are building our own vendor image, some necessary
        packages aren't being built and some props aren't being set.
        Additionally add missing components from system_ext.
    
        Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
        Co-authored-by: Josh Chasky <jchasky@gmail.com>
        Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
        Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
        Change-Id: I3e91822b336761cdecf3f6ebe416276dc0585785
    
    Author: "Christopher N. Hesse" <raymanfx@gmail.com>
    Date:   Fri Oct 26 10:00:54 2018 -0400
        b4s4: Generate symlinks
    
        This is a squash of the following:
    
        Author: Eamon Powell <eamonpowell@outlook.com>
        Date:   Mon Nov 2 15:03:12 2020 +1100
    
            b4s4: Add EGL symlinks
    
            Change-Id: I292a6180ba08d5a1a3ed9efb8aab6329e7f89fc8
    
        Author: Bruno Martins <bgcngm@gmail.com>
        Date:   Sat Sep 30 21:14:38 2017 +0300
    
            b4s4: Create RFS symlinks
    
            Change-Id: Ie9bc8ec251ee0cd0dcc8caab230662b6e93b72a5
    
        Author: Christopher N. Hesse <raymanfx@gmail.com>
        Date:   Sun Oct 29 13:52:21 2017 -0500
    
            b4s4: Add IMS + DMService symbolic links
    
            Change-Id: I105ce0e92533e478df66186f3b0fb59b420365c5
    
    Change-Id: Ic81e0fd5e1435120b3b8ada94c51ab2f91ec1aa2
    
    Author: Nolen Johnson <johnsonnolen@gmail.com>
    Date:   Fri Nov 5 15:11:16 2021 -0400
    
        b4s4: Set BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES
    
        * This is a pain in the ass.
    
        Change-Id: I9aefc29d7b03d76e0d13d4d1d3812ba73b800968
    
    Author: "P.Adarsh Reddy" <padarshr@codeaurora.org>
    Date:   Mon Mar 9 12:16:37 2020 +0530
    
        b4s4: Add build broken flags in BoardConfig.mk
    
        BUILD_BROKEN_USES_BUILD_COPY_HEADERS
    
        Change-Id: I7b115f5bb7990c438c595b4b1ff536598786d529
    
    Change-Id: Iae3563b716a628a0b64f4ec4e8403d1c08d795d3

--- device-lineage.mk
+++ device-lineage.mk
@@ -4,12 +4,110 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Build necessary packages for system
+PRODUCT_PACKAGES += \
+    libmediaplayerservice \
+    libstagefright_httplive:64 \
+    vendor.display.config@1.0
+
+# Build necessary packages for vendor
+PRODUCT_PACKAGES += \
+    chre \
+    ese_spi_nxp:64 \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.frameworks.stats@1.0.vendor \
+    android.hardware.authsecret@1.0.vendor \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    android.hardware.biometrics.fingerprint@2.2.vendor \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.identity_credential.xml \
+    android.hardware.identity-support-lib \
+    android.hardware.identity-support-lib.vendor \
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.neuralnetworks@1.0.vendor \
+    android.hardware.neuralnetworks@1.1.vendor \
+    android.hardware.neuralnetworks@1.2.vendor \
+    android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.oemlock@1.0.vendor \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.3.vendor \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.0.vendor \
+    android.hardware.weaver@1.0.vendor \
+    android.hardware.wifi@1.1.vendor \
+    android.hardware.wifi@1.2.vendor \
+    android.hardware.wifi@1.3.vendor \
+    android.hardware.wifi@1.4.vendor \
+    android.hardware.wifi@1.5.vendor \
+    android.system.net.netd@1.0.vendor \
+    android.system.net.netd@1.1.vendor \
+    hardware.google.bluetooth.sar@1.0 \
+    hardware.google.bluetooth.bt_channel_avoidance@1.0 \
+    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor \
+    hardware.google.bluetooth.sar@1.0.vendor \
+    hardware.google.light@1.0.vendor \
+    libcodec2_hidl_plugin:32 \
+    libavservices_minijail_vendor:32 \
+    libcodec2_hidl@1.0.vendor:32 \
+    libcodec2_vndk.vendor \
+    libcppbor.vendor:64 \
+    libcppbor_external \
+    libcppcose_rkp \
+    libdisplayconfig \
+    libdrm.vendor \
+    libhidltransport.vendor \
+    libhwbinder.vendor \
+    libjson \
+    libkeymaster_messages.vendor:64 \
+    libkeymaster_portable.vendor:64 \
+    libnetfilter_conntrack:64 \
+    libnfnetlink:64 \
+    libnos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnosprotos:64 \
+    libnos_transport:64 \
+    libprotobuf-cpp-full-vendorcompat \
+    libpuresoftkeymasterdevice.vendor:64 \
+    libsensorndkbridge:64 \
+    libsoft_attestation_cert.vendor:64 \
+    libteeui_hal_support.vendor:64 \
+    libtinycompress \
+    libtinyxml \
+    libwifi-hal:64 \
+    libwifi-hal-qcom \
+    libz_stable \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+    pixelatoms-cpp \
+    thermal_symlinks \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true  \
+    media.mediadrmservice.enable=true \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno
 
 # RCS
 PRODUCT_PACKAGES += \
