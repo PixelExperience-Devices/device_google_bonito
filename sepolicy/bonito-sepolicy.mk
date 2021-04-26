788982bce10915b2551bf8078b405c848995bef0
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-04-26 09:18:54 -0300

    b4s4: Include googlebattery sepolicy

--- sepolicy/bonito-sepolicy.mk
+++ sepolicy/bonito-sepolicy.mk
@@ -15,3 +15,4 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/bonito/sepolicy/system_ext/pri
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/ramdump/common
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_perf_diag
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
