ba1089fe5bf1557d481eec990f0bc1e8d15aff2f
jhenrique09 <jhsv09@gmail.com>
2022-03-28 13:38:28 +0000

    bonito: Update fingerprint from March release

--- aosp_bonito.mk
+++ aosp_bonito.mk
@@ -76,8 +76,8 @@ include device/google/bonito/device-custom.mk
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bonito \
-    PRIVATE_BUILD_DESC="bonito-user 12 SQ1A.220205.002 8010174 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 12 SP2A.220305.012 8177914 release-keys"
 
-BUILD_FINGERPRINT := google/bonito/bonito:12/SQ1A.220205.002/8010174:user/release-keys
+BUILD_FINGERPRINT := google/bonito/bonito:12/SP2A.220305.012/8177914:user/release-keys
 
 $(call inherit-product, vendor/google/bonito/bonito-vendor.mk)
